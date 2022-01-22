9be06a9d59b0e1d779ffc18c1e334e572a0cd6b6
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2022-01-22 19:59:07 +0300

    first commit

---
+++ Arka Plan Değiştirici/app.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const colors = ["black", "yellow", "green", "red", "#92eb34", "#7a34eb", "#3446eb","#eb346b", "#65eb34",  "#ebb134", "#a234eb", "#cc34eb"];
+
+
+button.addEventListener("click",changeBackground);
+
+let sıra = 0;
+
+function changeBackground() {
+    //rastgele renk
+  //  const rastgele = Math.floor(Math.random() * colors.length);  
+  //  const color = colors[rastgele];
+  //  console.log(rastgele ,color);
+  //  body.style.backgroundColor = color;
+
+    //sırayla renk
+    if(sıra == 12) sıra = 0;
+    const renk = colors[sıra];
+    sıra++;
+    body.style.backgroundColor = renk;
+}
